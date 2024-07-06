173c4e00796eba7e18c21fb3ddd74c1c6a4520fe
Sudip Das <sudip5428@gmail.com>
2024-07-06 20:46:43 +0530

    active link navber

---
+++ app/NavBar.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+const NavBar = () => {
+  const currentPage = usePathname();
+  console.log(currentPage);
+  const Links = [
+    { name: "Dashboard", href: "/" },
+    { name: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {Links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": currentPage === link.href,
+              "text-zinc-500": currentPage !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
