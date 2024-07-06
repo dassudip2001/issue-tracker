a7a1d860cc73f73bfecb8959034de1105ec3aaff
Sudip Das <sudip5428@gmail.com>
2024-07-06 21:39:10 +0530

    customize text editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
 import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const CreateIssuePage = () => {
   return (
     <>
       <div className="max-w-xl space-y-3">
         <TextField.Root placeholder="Enter Title"></TextField.Root>
-        <TextArea placeholder="Reply to comment…" />
+        <SimpleMDE placeholder="Reply to comment…" />
         <Button className="p-3 bg-blue-500 text-white rounded-md">
           Create Issue
         </Button>
